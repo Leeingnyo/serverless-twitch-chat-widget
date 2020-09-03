45f72687a9d6032030962865ca891aa58760c6a0
Leeingnyo <dyd1928@naver.com>
2020-09-04 05:03:28 +0900

    split configs, implement merge configs

--- config.js
+++ config.js
@@ -3,17 +3,59 @@ const pageConfigs = {
   readOnly: currentUrl.searchParams.get('read-only') === 'true'
 };
 
-// add default config
-
-const configs = {
+const defaultConfigs = {
   irc: {
     webSocketUrl: 'wss://irc-ws.chat.twitch.tv:443',
-    nick: NICKNAME,
+    nick: undefined,
     user: undefined,
-    token: TOKEN
+    token: undefined
   },
-  twitch: {
-    channel: CHANNEL_NAME
+  readOnly: false,
+  debug: {
+    rawMessages: false,
+    parsedMessages: false,
   },
-  readOnly: pageConfigs.readOnly
+}
+
+const mergeConfigs = (one, another, ...others) => {
+  if (others.length !== 0) {
+    return mergeConfigs(mergeConfigs(one, another), ...others);
+  }
+  /**
+   * @param {any} obj
+   * @returns {boolean}
+   */
+  const isObject = obj => !!obj && typeof obj === 'object';
+
+  /**
+   * deep merge two objects
+   * @param {Object} a
+   * @param {Object} b
+   */
+  const merge = (a, b) => {
+    const keysOfA = Object.keys(a);
+    const keysOfB = Object.keys(b);
+    const c = {};
+
+    keysOfA.forEach(keyFromA => {
+      if (keysOfB.includes(keyFromA)) {
+        c[keyFromA] = mergeConfigs(a[keyFromA], b[keyFromA]);
+      } else {
+        c[keyFromA] = a[keyFromA];
+      }
+    });
+    keysOfB.forEach(keyFromB => {
+      if (keysOfA.includes(keyFromB)) return;
+      c[keyFromB] = b[keyFromB];
+    });
+
+    return c;
+  }
+
+  if (typeof one === 'undefined' && typeof another === 'undefined') return;
+  if (typeof one === 'undefined') return another;
+  if (typeof another === 'undefined') return one;
+  if (isObject(one) && isObject(another)) return merge(one, another);
+  if (typeof one === typeof another) return another; // overwrite
+  throw Error('both configs have different type at same key');
 };
