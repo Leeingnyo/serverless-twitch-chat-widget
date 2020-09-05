52d4fc1970465cc156427320f0da0ac8beb31039
Leeingnyo <dyd1928@naver.com>
2020-09-06 02:46:52 +0900

    add image-json.example

---
+++ image-json.example.js
@@ -0,0 +1,8 @@
+// DO NOT DELETE the bellow defenition 바로 아래줄은 지우지 마세요
+const imageLinkMap =
+{
+  // recommand to edit with editor - set-image.html
+  // set-image.html 를 이용해 수정해주시기 바랍니다.
+  "~돌고래": "http://cfile202.uf.daum.net/R400x0/2142113F53CE18C3208CF5",
+  "[keyword]": "[url]"
+};
